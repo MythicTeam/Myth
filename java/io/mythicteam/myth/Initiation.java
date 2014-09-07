fe715245c0dec84c63d552f3a2b2513decc37e36
Lucas Olai <gamingtutsdk@gmail.com>
2014-09-07 13:04:12 +0200

    Added MythBlock and some other stuff

---
+++ java/io/mythicteam/myth/Initiation.java
@@ -0,0 +1,19 @@
+package io.mythicteam.myth;
+
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class Initiation {
+	protected static void loadPreInit(FMLPreInitializationEvent event) {
+		
+	}
+
+	protected static void loadInit(FMLInitializationEvent event) {
+		
+	}
+
+	protected static void loadPostInit(FMLPostInitializationEvent event) {
+		
+	}
+}
